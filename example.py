4888cc5e82aaf0bf6be269912c7d5f676c98a399
rajexp <rajanchauhan11@gmail.com>
2021-07-23 16:26:29 +0530

    Changed the package to parcel

--- example.py
+++ example.py
@@ -17,8 +17,8 @@ if __name__ == '__main__':
     _, content = api.get_all_content()
 
     # Example of quotation [ The search variables will be common for quotation and order API]
-    # You can add multiple packages below
-    packages = [{"type": "colis", "poids": 1, "longueur": 7, "largeur": 8, "hauteur":11, "value": 10.0, "description": "Newspapers"},
+    # You can add multiple parcels below
+    parcels = [{"type": "colis", "poids": 1, "longueur": 7, "largeur": 8, "hauteur":11, "value": 10.0, "description": "Newspapers"},
                 {"type": "pli", "poids": 3, "longueur": 6, "largeur": 8, "value": 3.0, "description": "Cloth"}]
     from_ = BoxtalAPI.Sender(title = "Mr", name = "Nikola", surname = "Tesla", email = "nikola@gmail.com", tel = "0606060606", 
                 country_code = "FR", postal_code = "44000", type = "particulier", company_name = "", city = "Paris", address = "Some address here")
@@ -32,8 +32,7 @@ if __name__ == '__main__':
     collection_date = "2021-07-25" # YYYY-MM-DD format
     operator = "UPSE"
     additional_parameters = {"code_contenu": content_code, "collecte": collection_date, "delai": delay, "operateur": operator}
-    # Preparing the package parameters which are of type <type>_N.<poids/longueur/largeur/hauteur>
-    _, quotation = api.get_quotation(from_, to, packages, additional_parameters)
+    _, quotation = api.get_quotation(from_, to, parcels, additional_parameters)
     with open("quotation.json", "w") as f:
         f.write(json.dumps(quotation))
     _, countries = api.get_countries()
@@ -55,14 +54,14 @@ if __name__ == '__main__':
     if optional_param:
         pickup_availability_time = "09:00" # Time in HH:MM format (24 hours format)
         delivery_availability_time = "19:00" # Time in HH:MM format (24 hours format)
-        final_date_of_delivery = "28-07-2021" # Final date DD-MM-YYYY till package must be delivered.
+        final_date_of_delivery = "28-07-2021" # Final date DD-MM-YYYY till parcels must be delivered.
         # Information to be selected from quotation API same service offer
         pickup_points_enum = list(filter(lambda x: x["code"] == "retrait.pointrelais", quotation["data"]["cotation"]["shipment"]["offer"][selected_offer_index]["mandatory_informations"]["parameter"]))[0].get("type", {})
         pickup_point_code = "" # Can be set based on enum availability
         dropoff_point_enum = list(filter(lambda x: x["code"] == "depot.pointrelais", quotation["data"]["cotation"]["shipment"]["offer"][selected_offer_index]["mandatory_informations"]["parameter"]))[0].get("type", {})
         dropoff_point_code = "" # Can be set based on enum availability
         additional_parameters.update({"disponibilite.HDE": pickup_availability_time, "disponibilite.HLE": delivery_availability_time, "livraison_imperative.DIL": final_date_of_delivery}) #, "retrait.pointrelais": pickup_point_code, "depot.pointrelais": dropoff_point_code})
-    _, order = api.create_order(from_, to, packages, additional_parameters)
+    _, order = api.create_order(from_, to, parcels, additional_parameters)
     # You should save the order object you get above for future reference purpose
     # Saving it in a order.json file here just for demo
     with open("order.json", "w") as f:
